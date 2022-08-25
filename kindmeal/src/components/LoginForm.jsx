cf97cb1f24ed39b4aa5a017fd281447ffda3f43e
Anshul-Singla <asingla2807@gmail.com>
2022-08-25 23:41:33 +0530

    60% work done of forms

--- kindmeal/src/components/LoginForm.jsx
+++ kindmeal/src/components/LoginForm.jsx
@@ -16,18 +16,24 @@ import {
     FormHelperText,
     Input,
     Text,
+    Center,
+    Flex,
+    Link
   } from '@chakra-ui/react'
+  import { TiSocialFacebookCircular } from 'react-icons/fa';
   function LoginForm({text , buttonStyles}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
-    console.log(useDisclosure())
+    // console.log(useDisclosure())
     return (
       <>
         <Button style={buttonStyles} onClick={onOpen}>{text}</Button>
-        <Modal isOpen={isOpen} onClose={onClose}  >
+        <Modal isOpen={isOpen} onClose={onClose} size='xl' isCentered  >
           <ModalOverlay />
-          <ModalContent border='1px' borderColor='blackAlpha.900' padding='20px 0'  >
-            <ModalHeader ml='20px' mr='20px' > 
-                <Image src='https://www.kindmeal.my/images/logo-kindmeal.png'      />
+          <ModalContent  padding='20px 0 '   >
+            <ModalHeader ml='20px'>
+                <Center>
+                  <Image src='https://www.kindmeal.my/images/logo-kindmeal.png'      />
+                </Center> 
             </ModalHeader>
             <ModalCloseButton />
             <ModalBody>
@@ -35,24 +41,30 @@ import {
                 <FormControl style={{ 
                         display:"Flex" ,
                         flexDirection:'column',
-                        justifyContent:'space-evenly' ,
+                        justifyContent:'center' ,
                         alignItems:'center' ,
-                        gap:'20px'
+                        gap:'20px',
+                        height:'250px'
                         }} >
                     <Text fontSize='2vw' color='#666666' >Member Login</Text>
                     <Input type='email' fontSize='1.5vw' placeholder='Your Email' w='80%'  />
                     <Input type='password' fontSize='1.5vw' placeholder='Your Password' w='80%'  />
-                    <Input bg='#2BB673' fontSize='1.75vw' color='#fff' w='60%' type='submit' placeholder='Login' />
+                    <Input bg='#2BB673' fontSize='1.75vw' color='#fff' w='60%' type='submit' h='' value='Login' p={1} />
                 </FormControl>
               </Box>
             </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button variant='ghost'>Secondary Action</Button>
-            </ModalFooter>
+            <Box borderTop='1px' borderColor='gray.400' ml={5} mr={5}  pt={5}>
+              <Center>
+                <Button pl={10} pr={10} colorScheme='facebook'  onClick={onClose}>Login With Facebook</Button>
+              </Center>
+            </Box>
+            <Box  m={5} >
+              <Flex justifyContent='space-between' >
+                <Link fontSize='1.35vw' fontWeight='500' > Forget password?</Link>
+                <Link fontSize='1.35vw' fontWeight='500' > Not a member? Sign up FREE! </Link>
+              </Flex>
+            </Box>
+           
           </ModalContent>
         </Modal>
       </>
--- kindmeal/src/components/Signup.jsx
+++ kindmeal/src/components/Signup.jsx
@@ -9,31 +9,63 @@ import {
     useDisclosure,
     Button,
     Image,
-    Text
+    Text,
+    Flex,
+    Link,
+    Box,
+    Spacer,
   } from '@chakra-ui/react'
+  // import { Link as ReachLink } from "@reach/router"
+import {   useNavigate,
+  Link as RouterLink,
+ } from 'react-router-dom';
   function SignUpForm({text , textStyle}) {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const navigate = useNavigate()
     return (
       <>
         <Text as='button' style={textStyle} onClick={onOpen}>{text}</Text>
   
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal  isOpen={isOpen} onClose={onClose} size='2xl' isCentered>
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>
-                <Image src='https://www.kindmeal.my/images/logo-kindmeal.png'      />
+              <Flex flexDirection='column' justifyContent='center' alignItems='center' >
+                <Image src='https://www.kindmeal.my/images/logo-kindmeal.png'  w='75%'    />
+                <Text fontSize='2vw' color='#666666' >Join KindMeal For FREE Now</Text>
+                <Text fontSize='1vw' color='#666666' >Please select the type of membership to proceed</Text>
+              </Flex>
             </ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <div>LIFE IS AWESOME........</div>
+              <Box as='button'  border="2px solid grey" rounded={5} p={1} mb={5} onClick={()=> navigate('/join')} >
+                <Flex gap={5}        >
+                  <Image src='https://www.kindmeal.my/images/join_normal.png'  w='50%' h='50%'       />
+                  <Box>
+                    <Text mb={2}  fontSize='1.5vw'color='#000' fontWeight='500' >Food Lover</Text>
+                    <Text fontSize='1.20vw'color='#666666' >Join FREE to enjoy yummy deals, share       reviews & meet our community of compassionate food lovers.
+                    </Text>
+                  </Box>
+                 </Flex>
+              </Box>
+              <Box as='button' border="2px solid grey" rounded={5} p={1} onClick={()=> navigate('/joinshop')} >
+                <Flex gap={5}        >
+                  <Image w='50%' h='50%' src='https://www.kindmeal.my/images/join_shop.png'         />
+                  <Box>
+                    <Text mb={2}  fontSize='1.5vw'color='#000' fontWeight='500' >Restaurant / Shop Owner</Text>
+                    <Text  >List your food outlet, create exciting vegetarian deals & showcase your delicious menu for FREE. Enjoy our yummy deals, share reviews & meet food lovers too.
+                    </Text>
+                  </Box>
+                 </Flex>
+              </Box>
             </ModalBody>
   
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button variant='ghost'>Secondary Action</Button>
-            </ModalFooter>
+              <Flex m={3}>
+                <Link fontSize='1.5vw'color='#666666' >Member Login</Link>
+                <Spacer/>
+                <Link fontSize='1.5vw'color='#666666' >Forget password?</Link>
+              </Flex>
+            
           </ModalContent>
         </Modal>
       </>
